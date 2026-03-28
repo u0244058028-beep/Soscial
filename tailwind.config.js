0abb1831c8389ec0f54687720968e93bce3e1a36
u0244058028-beep <u0244058028@gmail.com>
2026-03-28 12:26:45 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,17 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fade-in 0.6s ease-out',
+        'fade-in-up': 'fade-in-up 0.6s ease-out',
+        blob: 'blob 7s infinite',
+      },
+    },
+  },
+  plugins: [],
+}
